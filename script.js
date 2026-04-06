8cd4a17d0fb433ad22787e6131a66ffdbe180546
Diego Herrera <diegoherreraruiz2@gmail.com>
2026-04-05 22:55:37 -0500

    Primer commir proyecto con api fetch

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.querySelector("#input");
+const resultado = document.querySelector("#resultado");
+
+async function buscar() {
+  const pais = input.value.trim();
+  if (pais === "") return;
+
+  resultado.textContent = "Buscando...";
+
+  try {
+    const respuesta = await fetch(
+      `https://restcountries.com/v3.1/name/${pais}`,
+    );
+    const datos = await respuesta.json();
+    console.log(datos);
+
+    if (datos.status === 404) {
+      resultado.textContent = "País no encontrado. Intenta en inglés.";
+      return;
+    }
+
+    const p = datos[0];
+    resultado.innerHTML = `
+        <div class="card">
+            <img src="${p.flags.png}" alt="${p.name.common}">
+            <h2>${p.name.common}</h2>
+            <div class="dato"><span>Capital</span><span>${p.capital[0]}</span></div>
+            <div class="dato"><span>Población</span><span>${p.population.toLocaleString()}</span></div>
+            <div class="dato"><span>Región</span><span>${p.region}</span></div>
+        </div>
+`;
+  } catch (error) {
+    resultado.textContent = "Error al buscar el país.";
+  }
+}
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    buscar();
+  }
+});
